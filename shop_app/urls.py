e547b10ab42e82209a3ef5a56677caa90932ce1c
student <jasurabdibayev0@gmail.com>
2022-11-03 15:35:23 +0500

    templates static file upload

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home)
+    path('home/',home),
+    path('about/', about),
+    path('cart/', cart),
+    path('checkout/', checkout),
+    path('contact/', contact),
+    path('shop/', shop),
+    path('shop_single/', shop_single),
+    path('thankyou/', thankyou),
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -4,3 +4,43 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def shop(request):
+    return render(request,'shop.html')
+
+def shop_single(request):
+    return render(request,'shop-single.html')
+
+def thankyou(request):
+    return render(request,'thankyou.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def home(request):
+    return render(request,'index.html')
