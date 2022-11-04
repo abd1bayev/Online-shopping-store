092d5a551698b1455d731abd14d09fa98a08503b
student <jasurabdibayev0@gmail.com>
2022-11-04 15:02:50 +0500

    all

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
@@ -10,4 +12,5 @@ urlpatterns = [
     path('shop/', shop),
     path('shop-single/', shop_single),
     path('thankyou/', thankyou),
-]
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
